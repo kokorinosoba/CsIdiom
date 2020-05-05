4613db39a35451c38e1b79440441c6eab019fa2b
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-05-05 12:14:51 +0900

    :art: Chapter1 Ex1

--- Chapter1/Product.cs
+++ Chapter1/Product.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Chapter1
+namespace Chapter1sub
 {
     class Product
     {
--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -7,14 +7,8 @@ namespace Chapter1
     {
         static void Main(string[] args)
         {
-            List<object> objects = new List<object>();
-            objects.Add(new Employee { Name = "労働者" });
-            objects.Add(new Person { Name = "人" });
-            objects.Add(new Product(123, "かりんとう", 180));
-            foreach (var obj in objects)
-            {
-                Console.WriteLine(obj.Name);
-            }
+            Chapter1sub.Product karinto = new Chapter1sub.Product(123, "かりんとう", 180);
+            Console.WriteLine(karinto.GetPriceIncludingTax());
         }
     }
 }
