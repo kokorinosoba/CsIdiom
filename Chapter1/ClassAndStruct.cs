b6ff64a1dc1700d6fd374756687c3e01d877d19c
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-05-05 12:26:50 +0900

    :art: Chapter1 Ex2

---
+++ Chapter1/ClassAndStruct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter1
+{
+    class MyClass
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    struct MyStruct
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -7,8 +7,21 @@ namespace Chapter1
     {
         static void Main(string[] args)
         {
-            Chapter1sub.Product karinto = new Chapter1sub.Product(123, "かりんとう", 180);
-            Console.WriteLine(karinto.GetPriceIncludingTax());
+            MyClass  myClass  = new MyClass  { X = 1,  Y = 2  };
+            MyStruct myStruct = new MyStruct { X = 10, Y = 20 };
+            PrintObjects(myClass, myStruct);
+            PrintObjects(myClass, myStruct);
+        }
+
+        static void PrintObjects(MyClass myClass, MyStruct myStruct)
+        {
+            myClass.X *= 2;
+            myClass.Y *= 2;
+            myStruct.X *= 2;
+            myStruct.Y *= 2;
+
+            Console.WriteLine("MyClass X:" + myClass.X + "\tMyClass Y:" + myClass.Y);
+            Console.WriteLine("MyStruct X:" + myStruct.X + "\tMyStruct Y:" + myStruct.Y);
         }
     }
 }
