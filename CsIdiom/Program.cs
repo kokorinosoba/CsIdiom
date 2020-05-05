899196bbc1af18da6671d4f712cdf3acf0203de6
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-05-05 11:18:43 +0900

    initial commit

---
+++ CsIdiom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsIdiom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
